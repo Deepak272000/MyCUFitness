f8f0873a2a852ab2d154749a3c6a6c99b0c48e00
Deepak Chavan <deepaksunil.chavan@mail.concordia.ca>
2025-02-15 17:53:06 -0500

    new changes

--- MyCUFitness/urls.py
+++ MyCUFitness/urls.py
@@ -1,23 +1,24 @@
-"""
-URL configuration for MyCUFitness project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from users.views import home, about_view, contact_view, login_view, dashboard_view, enable_2fa_view
 
 urlpatterns = [
-        path('admin/', admin.site.urls),
-    path('api/', include('meal_plans.urls')),
+    path('admin/', admin.site.urls),  # Admin Panel
+    path('auth/', include('social_django.urls', namespace='social')),
+    path('', include('users.urls')),
+    path('', home, name='home'),  # Home Page
+    path("login/", login_view, name="login"),
+    path("dashboard/", dashboard_view, name="dashboard"),  # Fixed dashboard path
+    path('enable-2fa/', enable_2fa_view, name='enable_2fa'),
+    path('about-us/', about_view, name='about'),  # About Us Page
+    path('contact/', contact_view, name='contact'),  # Contact Page
+    path('api/users/', include('users.urls')),  # User Authentication & Profile
+    path('api/workouts/', include('workouts.urls')),  # Workout Management
+    path('api/auth/', include('django.contrib.auth.urls')),  # Django Built-in Auth
+    path('accounts/', include('allauth.urls')),  # Social Authentication
+
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
