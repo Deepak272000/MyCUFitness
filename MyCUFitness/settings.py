a19545d4da4ce6b61ca87181e2c7c224cb269e6b
Deepak Chavan <deepaksunil.chavan@mail.concordia.ca>
2025-02-15 18:01:09 -0500

    removed secrets from settings.py file

--- MyCUFitness/settings.py
+++ MyCUFitness/settings.py
@@ -103,8 +103,9 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "codecrafters2025coen@gmail.com"
-EMAIL_HOST_PASSWORD = "vdcu srvq kdat giyp"
+EMAIL_HOST_USER = config["EMAIL_HOST_USER"]
+EMAIL_HOST_PASSWORD = config["EMAIL_HOST_PASSWORD"]
+
 
 
 
